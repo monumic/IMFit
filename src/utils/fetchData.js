11abebeb533cc794540fbb44b260413970813168
monumic <128798766+monumic@users.noreply.github.com>
2024-07-26 13:54:52 +0530

    initial commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'e30eeb7046msh0e269dd30c0aabfp10baf5jsn6cc47664a3cb',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'e30eeb7046msh0e269dd30c0aabfp10baf5jsn6cc47664a3cb',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
